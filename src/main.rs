18479842557ac3d798576712c358772f403daa32
The Doge <alex.ren@gmail.com>
2023-08-18 22:42:28 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use astra_formats::{Book, TextBundle};
 use astra_formats::{Bundle, BundleFile};
+use pathdiff::diff_paths;
+use phf::phf_map;
 use std::fs::{self, File};
 use std::io::prelude::*;
 use std::path::Path;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 use clap::Parser;
 
@@ -39,17 +42,93 @@ const SUPPORTED_GAMEDATAS: &[(&str, &str)] = &[
     ("calculator", "Calculator"),
 ];
 
+static GAMEDATA_MAP: phf::Map<&'static str, &'static str> = phf_map! {
+    "person" => "Person",
+    "skill" => "Skill",
+    "shop" => "Shop",
+    "item" => "Item",
+    "god" => "God",
+    "job" => "Job",
+    "animset" => "AnimSet",
+    "params" => "Params",
+    "chapter" => "Chapter",
+    "assettable" => "AssetTable",
+    "animal" => "Animal",
+    "calculator" => "Calculator",
+};
 fn main() {
     let cli = Args::parse();
     let mod_path = cli.mod_path;
-
-    let is_romfs: bool = Path::new(&mod_path).join("romfs").is_dir();
+    let romfs_path = mod_path.join("romfs");
+    let mut target_path = mod_path.file_name().unwrap().to_str().unwrap().to_string();
+    target_path.push_str(" (Cobalt)");
+    println!("Mod name: {}", &target_path);
+    let is_romfs: bool = romfs_path.is_dir();
 
     if !is_romfs {
         println!("This isn't a romfs folder, so I can't help you here.");
     }
 
-    check_gamedata_folder(&mod_path);
+    fs::create_dir_all(Path::new(&target_path).join("patches/xml"));
+    fs::create_dir_all(Path::new(&target_path).join("Data"));
+
+    for entry in WalkDir::new(Path::new(&mod_path).join("romfs"))
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let path = entry.path();
+        let relative_path = diff_paths(path, &romfs_path).unwrap();
+        println!("{}", relative_path.display());
+        if path.is_dir() {
+            fs::create_dir_all(Path::new(&target_path).join(relative_path));
+            continue;
+        }
+
+        let file_name = entry
+            .file_name()
+            .to_str()
+            .unwrap()
+            .trim_end_matches(".xml.bundle");
+        // check if file is in SUPPORTED_GAMEDATAS
+        // if it is, then we need to load it and do stuff
+        // if it isn't, then we need to copy it over
+        let supported_gamedata = GAMEDATA_MAP.contains_key(file_name);
+        println!("Is {} supported: {}", file_name, supported_gamedata);
+        if supported_gamedata {
+            let new_name = GAMEDATA_MAP.get(file_name).unwrap();
+            println!("Migrating {} to {}", file_name, new_name);
+            migrate_gamedata(&path.to_path_buf(), new_name, &target_path);
+        } else {
+            println!("Copying {} to {}", file_name, &target_path);
+            fs::copy(path, Path::new(&target_path).join(relative_path)).expect("died");
+        }
+    }
+
+    // check_gamedata_folder(&mod_path);
+}
+
+fn migrate_gamedata(path: &PathBuf, new_name: &str, target_path: &str) {
+    let my_bundle = TextBundle::load(path);
+
+    match my_bundle {
+        Ok(mut bundle) => {
+            println!("Bundle loaded successfully.");
+            let my_result = bundle.take_raw().unwrap();
+            // println!("Bundle: {:?}", my_result);
+            let mut file = File::create(
+                Path::new(target_path)
+                    .join("patches")
+                    .join("xml")
+                    .join(new_name)
+                    .with_extension("xml"),
+            )
+            .unwrap();
+            file.write_all(my_result.as_slice()).expect("died");
+        }
+        Err(e) => {
+            println!("Error loading bundle: {:?}", e);
+        }
+    }
 }
 
 fn check_gamedata_folder(mod_path: &PathBuf) -> bool {
@@ -69,7 +148,7 @@ fn check_gamedata_folder(mod_path: &PathBuf) -> bool {
         .partition(|entry| {
             let binding = entry.file_name();
             SUPPORTED_GAMEDATAS.iter().any(|(name, _)| {
-                let file_name = format!("{}.xml.bundle", name);
+                let file_name: String = format!("{}.xml.bundle", name);
                 &file_name == &binding.unwrap().to_str().unwrap()
             })
         });
